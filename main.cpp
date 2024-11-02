fa00d14d991fdc1e87df8ff4d250a563c141f2b8
Montaser Amoor <montaser51515@gmail.com>
2024-11-02 17:49:59 +0300

    added file cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#define endl '\n'
+
+void Solution()
+{
+    std::cout << "Solution" << endl;
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    Solution();
+
+    return 0;
+}
